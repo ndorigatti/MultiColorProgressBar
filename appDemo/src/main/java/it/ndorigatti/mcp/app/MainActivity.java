803e03cf5bbd13d51b9c478c7146185d187bb147
Nicola Dorigatti <turricatch@gmail.com>
2014-07-30 19:22:28 +0200

    Added License (Apache 2.0)

--- appDemo/src/main/java/it/ndorigatti/mcp/app/MainActivity.java
+++ appDemo/src/main/java/it/ndorigatti/mcp/app/MainActivity.java
@@ -1,5 +1,20 @@
-package it.ndorigatti.mcp.app;
+/*******************************************************************************
+ * Copyright (C) 2014 Nicola Dorigatti
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ ******************************************************************************/
 
+package it.ndorigatti.mcp.app;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
@@ -197,7 +212,6 @@ public class MainActivity extends Activity {
                 .singleShot(140586L)
                 .build();
         scv.setShouldCentreText(true);
-
 
     }
 }
